038c214700a5765cefb0cf2f651c9164881362ed
Davit <90.davitmanukyan@mail.ru>
2021-12-02 00:47:44 +0400

    without using the third one

--- script.js
+++ script.js
@@ -9,9 +9,27 @@
 // alert(n);
 
 
-let a = 10, b = 3;
-a = 3;
-b = 10;
+// let a = 10, b = 3;
+// a = 3;
+// b = 10;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+let a = 5, b = 6;
+[a, b] = [b, a];
+console.log(`${a} ${b}`);
+
+
+var a1 = 1,
+    b1 = 2,
+    tmp;
+tmp = a1;
+a1 = b1;
+b1 = tmp;
+console.log(`${a1} ${b1}`);
+
