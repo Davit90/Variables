f44d2f4bb7206e9133bcd2d61f34bb2ba569400f
Davit <90.davitmanukyan@mail.ru>
2021-12-02 00:41:54 +0400

    Swap two variables

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
 // alert( admin );
 
 
-let i = 10;
-let n = i++ % 5;
-alert(n);
+// let i = 10;
+// let n = i++ % 5;
+// alert(n);
+
+
+let a = 10, b = 3;
+a = 3;
+b = 10;
+
+console.log(a);
+console.log(b);
