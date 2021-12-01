4e21acac382501ffe0cdb2648e358be182cba6a1
Davit <90.davitmanukyan@mail.ru>
2021-12-02 00:38:44 +0400

    what will be output

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let admin, name;
-name = "John";
-admin = name;
-alert( admin );
+// let admin, name;
+// name = "John";
+// admin = name;
+// alert( admin );
+
+
+let i = 10;
+let n = i++ % 5;
+alert(n);
